9fc9d4a5acc2904545459d4e736e15b4f972fd3c
Phil Borlin <phil@borlin.net>
2018-05-25 08:04:25 -0700

    Refactor out the result part of WordCount into WordCountResults

--- ui/src/WordCount.js
+++ ui/src/WordCount.js
@@ -2,23 +2,13 @@ import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import axios from 'axios'
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
+import WordCountResults from './WordCountResults';
 
 class WordCount extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      openUploadModal: false,
-      files: [],
       totalWordCount: 0,
       counts: [],
     };
@@ -46,7 +36,6 @@ class WordCount extends Component {
       <div>
         <input
           accept="text/plain"
-          className={this.props.input}
           style={inputStyle}
           id="file-upload"
           type="file"
@@ -58,33 +47,7 @@ class WordCount extends Component {
           </Button>
         </label>
 
-        <Grid container justify="space-around">
-          <Card>
-            <CardContent>
-              <Typography variant="headline" component="h2">
-                Total Word Count: {this.state.totalWordCount}
-              </Typography>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Word</TableCell>
-                    <TableCell numeric>Count</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {this.state.counts.map(n => {
-                    return (
-                      <TableRow key={n.word}>
-                        <TableCell component="th" scope="row">{n.word}</TableCell>
-                        <TableCell numeric>{n.count}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </Grid>
+        <WordCountResults totalWordCount={this.state.totalWordCount} counts={this.state.counts}/>
       </div>
     );
   }
