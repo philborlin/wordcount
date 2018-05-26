9b0b8b3c125676d42d662d79b10fc307609b3fca
Phil Borlin <phil@borlin.net>
2018-05-25 22:02:30 -0700

    Fix Upload so it can be called more than once

--- ui/src/WordCount.js
+++ ui/src/WordCount.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import axios from 'axios'
 
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
 import WordCountResults from './WordCountResults';
 
 class WordCount extends Component {
@@ -11,18 +17,30 @@ class WordCount extends Component {
     this.state = {
       totalWordCount: 0,
       counts: [],
+      shouldHide: true,
     };
   }
 
-  handleChange(selectorFiles) {
+  clear() {
+    this.setState({totalWordCount : 0, counts : [], shouldHide : true})
+  }
+
+  handleChange(e) {
+    const selectorFiles = e.target.files
     const formData = new FormData();
+    e.persist()
     Array.from(selectorFiles).forEach(file => {
       formData.append(file.name, file);
     });
 
     return axios.post("/wordcount", formData, {
     }).then(response => {
-      this.setState({totalWordCount : response.data.totalWordCount, counts : response.data.counts})
+      this.setState({
+        totalWordCount : response.data.totalWordCount,
+        counts : response.data.counts,
+        shouldHide : false,
+      })
+      e.target.value = null;
     })
   }
 
@@ -34,20 +52,44 @@ class WordCount extends Component {
 
     return (
       <div>
-        <input
-          accept="text/plain"
-          style={inputStyle}
-          id="file-upload"
-          type="file"
-          onChange={ (e) => this.handleChange(e.target.files) }
-        />
-        <label htmlFor="file-upload">
-          <Button variant="raised" component="span">
-            Upload
-          </Button>
-        </label>
+        <Grid container justify="space-around" spacing={24}>
+          <Grid item xs={10}>
+            <Card>
+              <CardContent>
+                <Typography variant="headline" component="h2">
+                  Word Count
+                </Typography>
+                <Typography component="h2">
+                  Click the Upload button to upload a file of type text/plain
+                  and we'll analyze the file and give you a report of all the
+                  words in your file.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <input
+                  accept="text/plain"
+                  style={inputStyle}
+                  id="file-upload"
+                  type="file"
+                  onChange={ (e) => this.handleChange(e) }
+                />
+                <label htmlFor="file-upload">
+                  <Button component="span">
+                    Upload
+                  </Button>
+                </label>
 
-        <WordCountResults totalWordCount={this.state.totalWordCount} counts={this.state.counts}/>
+                <Button component="span" onClick={ (e) => this.clear() }>
+                  Clear
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs={6} style={this.state.shouldHide ? { display: 'none' } : {} }>
+            <WordCountResults totalWordCount={this.state.totalWordCount} counts={this.state.counts}/>
+          </Grid>
+        </Grid>
       </div>
     );
   }
--- ui/src/WordCountResults.js
+++ ui/src/WordCountResults.js
@@ -8,38 +8,35 @@ import TableRow from '@material-ui/core/TableRow';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 
 class WordCountResults extends Component {
   render() {
     return (
-      <Grid container justify="space-around">
-          <Card>
-            <CardContent>
-              <Typography variant="headline" component="h2">
-                Total Word Count: {this.props.totalWordCount}
-              </Typography>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Word</TableCell>
-                    <TableCell numeric>Count</TableCell>
+      <Card>
+        <CardContent>
+          <Typography variant="headline" component="h2">
+            Total Word Count: {this.props.totalWordCount}
+          </Typography>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Word</TableCell>
+                <TableCell numeric>Count</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.props.counts.map(n => {
+                return (
+                  <TableRow key={n.word}>
+                    <TableCell component="th" scope="row">{n.word}</TableCell>
+                    <TableCell numeric>{n.count}</TableCell>
                   </TableRow>
-                </TableHead>
-                <TableBody>
-                  {this.props.counts.map(n => {
-                    return (
-                      <TableRow key={n.word}>
-                        <TableCell component="th" scope="row">{n.word}</TableCell>
-                        <TableCell numeric>{n.count}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </CardContent>
-          </Card>
-        </Grid>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
     );
   }
 }
