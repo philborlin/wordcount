e9f01a33479a2e87104f22833f5b8117fbf8dc54
Phil Borlin <phil@borlin.net>
2018-05-25 22:51:37 -0700

    Clean up front-end

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,14 +1,8 @@
 import React, {Component} from 'react';
 
-import './App.css';
 import WordCount from './WordCount.js';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {title: ''};
-  }
-
   render() {
     return (
       <div>
--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 describe('App tests', () => {
-
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<App />, div);
